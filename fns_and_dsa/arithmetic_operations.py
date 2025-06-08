6e6ca19fb765eb23bda0d14b87751f0ea95ad9a2
Arinzayyy - Kris <103210483+Arinzayy-1234@users.noreply.github.com>
2025-06-08 23:15:57 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,14 @@
 
 
 def perform_operation(num1, num2, operation):
-    if action == "add":
-        return x + y
-    elif action == "subtract":
-        return x - y
-    elif action == "multiply":
-        return x * y
-    elif action == "divide":
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
         if y == 0:
-            print("yasd")
             return None
         else:
             return x / y
