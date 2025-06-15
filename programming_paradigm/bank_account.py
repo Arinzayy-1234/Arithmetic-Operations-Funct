044089be97102e016da071057feb86d69f6e74e3
Arinzayyy - Kris <103210483+Arinzayy-1234@users.noreply.github.com>
2025-06-15 23:42:59 +0100

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -19,7 +19,7 @@ class BankAccount:
         
     
     def display_balance(self):
-        print (f'Current Balance: ${self.account_balance}')
+        print (f'Current Balance: ${self.account_balance}.00')
         
     
     
