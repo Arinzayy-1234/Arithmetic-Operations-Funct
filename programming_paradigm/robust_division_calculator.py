5465425a8927c582c3aadf62eada8649164b5ed4
Arinzayyy - Kris <103210483+Arinzayy-1234@users.noreply.github.com>
2025-06-16 00:15:31 +0100

    Updated the robust division calculator to handle zero division errors :wq

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,19 @@
+
+
+def safe_divide(numerator, denominator):
+    
+    num_float = float(numerator)
+    denum_flaot = float(denominator)
+    try:
+        return f'The result of the division is {num_float/denum_float}'
+        
+    
+    except ZeroDivisionError:
+        return 'Error: Cannot divide by zero.'
+    
+    except ValueError:
+        return 'Error: Please enter numeric values only.'
+    
+    
+        
+        
